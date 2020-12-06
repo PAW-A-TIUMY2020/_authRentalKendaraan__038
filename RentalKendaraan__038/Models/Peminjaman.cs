a71554383728675b1bbdbf90032b5337e7811dc3
Erwin Santosa <erwincoc232@gmail.com>
2020-12-06 11:09:17 +0700

    Add project files.

---
+++ RentalKendaraan__038/Models/Peminjaman.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentalKendaraan__038.Models
+{
+    public partial class Peminjaman
+    {
+        public Peminjaman()
+        {
+            Pengembalian = new HashSet<Pengembalian>();
+        }
+
+        public int IdPeminjaman { get; set; }
+
+        [Required(ErrorMessage = "Tanggal peminjaman harus diisi")]
+        public DateTime? TglPeminjaman { get; set; }
+
+        [Required(ErrorMessage = "Kendaraan tidak boleh kosong")]
+        public int? IdKendaraan { get; set; }
+
+        [Required(ErrorMessage = "Customer tidak boleh kosong")]
+        public int? IdCustomer { get; set; }
+
+        [Required(ErrorMessage = "Jaminan tidak boleh kosong")]
+        public int? IdJaminan { get; set; }
+
+        [Required(ErrorMessage = "Biaya tidak boleh kosong")]
+        public int? Biaya { get; set; }
+
+        public Customer IdCustomerNavigation { get; set; }
+        public Jaminan IdJaminanNavigation { get; set; }
+        public Kendaraan IdKendaraanNavigation { get; set; }
+        public ICollection<Pengembalian> Pengembalian { get; set; }
+    }
+}
